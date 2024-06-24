69f4af68093f30a71bbd0c645d04e07ba8d668cb
lorain <lorain@MacBook-Pro.local>
2024-06-24 13:49:59 +0800

    feat: 🎸 update

---
+++ search-github-trending.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Search Github Trending
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+# @raycast.packageName Search Github Trending
+
+# Documentation:
+# @raycast.description Search Github Trending
+# @raycast.author wings_lorain
+# @raycast.authorURL https://raycast.com/wings_lorain
+
+echo "Hello World! Argument1 value: "$1""
+
