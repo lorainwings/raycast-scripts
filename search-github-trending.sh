0d1401a12152f69d9201d13fe198c873ccff677c
lorain <lorain@MacBook-Pro.local>
2024-06-24 14:27:51 +0800

    feat: 🎸 update

--- search-github-trending.sh
+++ search-github-trending.sh
@@ -3,17 +3,29 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Search Github Trending
-# @raycast.mode compact
+# @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 🤖
-# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+# @raycast.icon 🔍
+# @raycast.argument1 { "type": "text", "placeholder": "Language" }
+# @raycast.argument2 { "type": "dropdown", "placeholder": "Data Range", "data": [{ "title": "Today", "value": "daily" }, { "title": "This Week", "value": "weekly" }, { "title": "This Month", "value": "monthly" }] }
+# @raycast.argument3 { "type": "text", "placeholder": "Spoken Language", "optional": true }
 # @raycast.packageName Search Github Trending
 
 # Documentation:
 # @raycast.description Search Github Trending
 # @raycast.author wings_lorain
 # @raycast.authorURL https://raycast.com/wings_lorain
+set -e;
 
-echo "Hello World! Argument1 value: "$1""
+language=$1;
+since=$2;
+spoken_language_code=$3;
 
+# -z 判断变量值的长度为空
+if [ -z "$spoken_language_code" ]; then
+    open "https://github.com/trending/$language?since=$since";
+else
+    open "https://github.com/trending/$language?since=$since&spoken_language_code=$spoken_language_code"
+fi
+exit 0;
