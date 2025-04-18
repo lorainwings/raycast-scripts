16596b070e75a200b5f40e28354c26e8fe7ecfd3
lorain <lorainwings@gmail.com>
2025-04-19 00:47:20 +0800

    feat: 🎸 update

--- fake-cursor-pro.sh
+++ fake-cursor-pro.sh
@@ -14,7 +14,35 @@
 # @raycast.author wings_lorain
 # @raycast.authorURL https://raycast.com/wings_lorain
 
-curl -fsSL https://raw.githubusercontent.com/yuaotian/go-cursor-help/master/scripts/install.sh | sudo bash
+set -e
 
+echo "⚙️ Start to Fake Cursor VIP!"
 
+token=$1
 
+check() {
+  # -n 检查字符串是否不为空, 不为空进入条件
+  if [[ -n "$token" ]]; then
+    echo "$token" >cache/.token
+  else
+    # -f 判断文件是否存在
+    if [[ -f "cache/.token" ]]; then
+      token=$(<cache/.token)
+    fi
+    # -z 检查字符串是否为空, 空值进入条件
+    if [[ -z "$token" ]]; then
+      echo "⛔️ 请使用Root权限密码!"
+      exit 1
+    fi
+  fi
+}
+
+main() {
+  echo $token | curl -fsSL https://raw.githubusercontent.com/yuaotian/go-cursor-help/master/scripts/install.sh | sudo -S bash
+}
+
+check
+
+main
+
+echo "✅ Fake Cursor Vip Done!"
